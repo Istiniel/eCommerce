4bf244b63d21b4bfcddc1f2d65298302eeca6ce3
Dmitry Konev <dkon70@inbox.ru>
2023-08-14 23:58:32 +0700

    feat: add about us page route

---
+++ src/pages/AboutUs/index.tsx
@@ -0,0 +1,11 @@
+// import styles from './AboutUs.module.scss';
+
+const AboutUs = () => {
+    return (
+        <>
+            <h1>About us</h1>
+        </>
+    );
+}
+
+export default AboutUs;
