c41d3314daeffbbf6cb0861403611ee7db5cf643
shoxa <sllx.ok@gmail.com>
2023-09-11 12:50:09 +0500

    refactor: take out h2

--- src/pages/Basket/index.tsx
+++ src/pages/Basket/index.tsx
@@ -6,45 +6,47 @@ import Button from '../../shared/ui/Button';
 const Basket = () => {
   return (
     <section className={styles.section}>
-      <div className={styles.leftColumn}>
-        <h2 className={styles.title}>Order Summary</h2>
-        <div className={styles.basketCardContainer}>
-          <BasketCard />
-          <BasketCard />
-          <BasketCard />
-          <BasketCard />
+      <h2 className={styles.title}>Order Summary</h2>
+      <div className={styles.container}>
+        <div className={styles.leftColumn}>
+          <div className={styles.basketCardContainer}>
+            <BasketCard />
+            <BasketCard />
+            <BasketCard />
+            <BasketCard />
+          </div>
         </div>
-      </div>
-      <div className={styles.rightColumn}>
-        <div className={styles.orderSummaryContainer}>
-          <div className={styles.promoContainer}>
-            <form className={styles.submitPromoForm}>
-              <Input
-                labelClassName={styles.promoInput}
-                type="text"
-                name="searchFilter"
-                placeholder="Promo code"
-                error=""
-                value=""
-                onChange={() => {}}
-                noLabel
-              />
-              <Button buttonType="solid" className={styles.promoSubmitButton}>
-                Apply
-              </Button>
-            </form>
+        <div className={styles.rightColumn}>
+          <div className={styles.orderSummaryContainer}>
+            <div className={styles.promoContainer}>
+              <form className={styles.submitPromoForm}>
+                <Input
+                  labelClassName={styles.promoInput}
+                  type="text"
+                  name="searchFilter"
+                  placeholder="Promo code"
+                  error=""
+                  value=""
+                  onChange={() => {}}
+                  noLabel
+                />
+                <Button buttonType="solid" className={styles.promoSubmitButton}>
+                  Apply
+                </Button>
+              </form>
+            </div>
+            <div className={styles.subTotalContainer}>
+              <span>Subtotal</span>
+              <span>100$</span>
+            </div>
+            <div className={styles.totalContainer}>
+              <span>Total</span>
+              <span>100$</span>
+            </div>
+            <Button buttonType="outlined" className={styles.checkoutButton}>
+              Checkout
+            </Button>
           </div>
-          <div className={styles.subTotalContainer}>
-            <span>Subtotal</span>
-            <span>100$</span>
-          </div>
-          <div className={styles.totalContainer}>
-            <span>Total</span>
-            <span>100$</span>
-          </div>
-          <Button buttonType="outlined" className={styles.checkoutButton}>
-            Checkout
-          </Button>
         </div>
       </div>
     </section>
