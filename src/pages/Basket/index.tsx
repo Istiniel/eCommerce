a53cd63edffa77751818adc5d2817075e546d6ed
shoxa <sllx.ok@gmail.com>
2023-09-11 23:11:15 +0500

    feat: create and add Summary UI Component

--- src/pages/Basket/index.tsx
+++ src/pages/Basket/index.tsx
@@ -1,7 +1,6 @@
 import styles from './Basket.module.scss';
 import BasketCard from '../../shared/ui/BasketCard';
-import { Input } from '../../shared/ui/Input';
-import Button from '../../shared/ui/Button';
+import Summary from './ui/Summary';
 
 const Basket = () => {
   return (
@@ -17,36 +16,7 @@ const Basket = () => {
           </div>
         </div>
         <div className={styles.rightColumn}>
-          <div className={styles.orderSummaryContainer}>
-            <div className={styles.promoContainer}>
-              <form className={styles.submitPromoForm}>
-                <Input
-                  labelClassName={styles.promoInput}
-                  type="text"
-                  name="searchFilter"
-                  placeholder="Promo code"
-                  error=""
-                  value=""
-                  onChange={() => {}}
-                  noLabel
-                />
-                <Button buttonType="solid" className={styles.promoSubmitButton}>
-                  Apply
-                </Button>
-              </form>
-            </div>
-            <div className={styles.subTotalContainer}>
-              <span>Subtotal</span>
-              <span>100$</span>
-            </div>
-            <div className={styles.totalContainer}>
-              <span>Total</span>
-              <span>100$</span>
-            </div>
-            <Button buttonType="outlined" className={styles.checkoutButton}>
-              Checkout
-            </Button>
-          </div>
+          <Summary />
         </div>
       </div>
     </section>
---
+++ src/pages/Basket/ui/Summary/index.tsx
@@ -0,0 +1,40 @@
+import Button from '../../../../shared/ui/Button';
+import { Input } from '../../../../shared/ui/Input';
+import styles from './Summary.module.scss';
+
+const Summary = () => {
+  return (
+    <div className={styles.orderSummaryContainer}>
+      <div className={styles.promoContainer}>
+        <form className={styles.submitPromoForm}>
+          <Input
+            labelClassName={styles.promoInput}
+            type="text"
+            name="searchFilter"
+            placeholder="Promo code"
+            error=""
+            value=""
+            onChange={() => {}}
+            noLabel
+          />
+          <Button buttonType="solid" className={styles.promoSubmitButton}>
+            Apply
+          </Button>
+        </form>
+      </div>
+      <div className={styles.subTotalContainer}>
+        <span>Subtotal</span>
+        <span>100$</span>
+      </div>
+      <div className={styles.totalContainer}>
+        <span>Total</span>
+        <span>100$</span>
+      </div>
+      <Button buttonType="outlined" className={styles.checkoutButton}>
+        Checkout
+      </Button>
+    </div>
+  );
+};
+
+export default Summary;
